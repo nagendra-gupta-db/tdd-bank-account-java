b5fd28586d44bbd4dc1150174fd7845c3c36c3b3
nagendra-gupta-db <nagendrankg@gmail.com>
2021-09-15 13:01:04 +0100

    Added Deposit Feature

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,12 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+
+    public Double balance = 0.0;
+
+    public void deposit(double deposit) {
+
+        balance += deposit;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+
+        account.deposit(100.0);
+
+        Assertions.assertEquals(100.0, account.balance);
     }
 }
